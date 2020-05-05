c449106ac9b39eb1f6e6a16ead76954b70536409
Geonguk Han <umanking@gmail.com>
2020-05-05 14:55:47 +0900

    @ManyToOne fetch lazy로 설정

--- src/main/java/com/example/demo/domain/OrderItem.java
+++ src/main/java/com/example/demo/domain/OrderItem.java
@@ -20,12 +20,12 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
     //todo: 1:N 인데 List를 안받네?
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
